7bddec5e8676e81320b7e3d70fa9be84472d71b0
Warre-python <warreodecock@gmail.com>
2025-07-14 18:42:55 +0000

    wip

---
+++ Counter.java
@@ -0,0 +1,10 @@
+public class Counter {
+    public static void main(String[] args) {
+        boolean run = true;
+        int counter = 0;
+        while (run) {
+            counter += 1;
+            System.out.println(counter);
+        }
+    }
+}
--- Start.java
+++ Start.java
@@ -1,5 +1,7 @@
 public class Start {
     public static void main(String[] args) {
-        Prime.calculatePrimeNumbers(args);
+        //Prime.calculatePrimeNumbers(args);
+        Counter counter = new Counter();
+        counter.main(args);
     }
 }

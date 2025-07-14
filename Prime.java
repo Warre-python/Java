f0753463e880f048a5fc325d3f01533c962744ff
Warre-python <warreodecock@gmail.com>
2025-07-14 03:58:14 +0000

    wip

--- Prime.java
+++ Prime.java
@@ -6,26 +6,36 @@ public class Prime {
         System.out.println("Welke int?");
         Scanner scanner = new Scanner(System.in);
         int lenght = scanner.nextInt();
-        int score = 0;
+
         long start = System.currentTimeMillis();
         for (int i = 1; i <= lenght; i++) {
-            for (int x = 1; x <= i; x ++) {
-                if (i % x == 0) {
-                    ++score;
-                    
-                }
-            }
-
-            if (score == 2) {
+            
+            if (isPrime(args, i)) {
                 System.out.println(i);
-
             }
-            score = 0;
         }
         long stop = System.currentTimeMillis();
         long time = stop - start;
         System.out.println("In " + time + " milliseconds!");
         scanner.close();
 
+    }
+    public static boolean isPrime(String[] args, int i) {
+        int score = 0;
+        for (int x = 1; x <= i; x++) {
+            if (i % 2 == 0) {
+                break;
+            }
+            if (i % x == 0) {
+                score++;
+            }
+
+        }
+        if (score == 2) {
+            return true;
+        } else {
+            return false;
+        }
+        
     }
 }
